158edd842f22dd01c3f57c59d754fca0b6c3b3bf
OracleBallpark <nystromjp@gmail.com>
2020-10-05 14:01:36 -0700

    Initial commit

---
+++ src/main/java/net/fabricmc/example/Main.java
@@ -0,0 +1,25 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.Option;
+
+public class Main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+		System.out.println("Banning Auto-Jump...");
+
+		ClientLifecycleEvents.CLIENT_STARTED.register(minecraftClient -> {
+			MinecraftClient.getInstance().options.autoJump=false;
+		});
+
+		ClientTickEvents.START_CLIENT_TICK.register(client -> {
+			if (MinecraftClient.getInstance().player!=null){
+			}
+		});
+
+	}
+}
